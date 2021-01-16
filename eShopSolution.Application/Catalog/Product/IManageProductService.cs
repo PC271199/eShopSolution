a606460e6293d2e7efd2c3f4174dc2a3e857e7df
PC271199 <lyphuoccong2711@gmail.com>
2021-01-16 19:42:26 +0700

    add category/product service

---
+++ eShopSolution.Application/Catalog/Product/IManageProductService.cs
@@ -0,0 +1,21 @@
+﻿using eShopSolution.Application.Catalog.Product.Dtos;
+using eShopSolution.Application.Catalog.Product.Dtos.Manage;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Product
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+        Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+        Task AddViewCount(int productId);
+        Task<PageResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+    }
+}
---
+++ eShopSolution.Application/Catalog/Product/IPublicProductService.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.Application.Catalog.Product.Dtos;
+using eShopSolution.Application.Catalog.Product.Dtos.Public;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Product
+{
+    public interface IPublicProductService
+    {
+        Task<PageResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request);
+    }
+}
