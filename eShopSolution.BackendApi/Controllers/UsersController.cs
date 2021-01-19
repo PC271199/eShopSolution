1255a75fe5484acb3a7bdfbdae9c0cb940abca2a
PC271199 <lyphuoccong2711@gmail.com>
2021-01-19 22:26:08 +0700

    add loginapi

---
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.Application.System.Users;
+using eShopSolution.ViewModel.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+            Console.WriteLine("aaaa");
+        }
+        [HttpPost("Authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            var resultToken = await _userService.Authenticate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+            return Ok(resultToken);
+        }
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userService.Register(request);
+            if (result==false)
+            {
+                return BadRequest("Register is unsuccessful");
+            }
+            return Ok();
+        }
+    }
+}
